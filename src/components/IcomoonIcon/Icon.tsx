66867ff4bce6af4333e4d116ef063469b8f36fca
Martin Bonchev <martin.bonchevv@gmail.com>
2023-05-01 22:27:57 +0300

    feat: add cursor pointer to icon

--- src/components/IcomoonIcon/Icon.tsx
+++ src/components/IcomoonIcon/Icon.tsx
@@ -2,19 +2,20 @@ import IcomoonReact from "icomoon-react";
 import selection from "../../assets/icons/selection.json";
 
 interface IconProps {
-    color?: string;
-    size: string | number;
-    icon: string;
+  color?: string;
+  size: string | number;
+  icon: string;
 }
 
 export default function Icon(props: IconProps) {
-    const { color, size, icon } = props;
-    return (
-        <IcomoonReact
-            iconSet={selection}
-            color={color}
-            size={size}
-            icon={icon}
-        />
-    );
+  const { color, size, icon } = props;
+  return (
+    <IcomoonReact
+      iconSet={selection}
+      className="cursor-pointer"
+      color={color}
+      size={size}
+      icon={icon}
+    />
+  );
 }
