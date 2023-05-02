d476af2176e49349e3ac9a243e2a00d755be923c
Martin Bonchev <martin.bonchevv@gmail.com>
2023-05-02 17:14:28 +0300

    feat: forward ref component

--- src/components/InputFileField/InputFileField.tsx
+++ src/components/InputFileField/InputFileField.tsx
@@ -1,4 +1,5 @@
 import { classNames } from "@root/utils/functions/utils";
+import { LegacyRef, forwardRef } from "react";
 import type { FieldError } from "react-hook-form";
 
 interface InputFileProps extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -11,57 +12,71 @@ interface InputFileProps extends React.InputHTMLAttributes<HTMLInputElement> {
   descriptionClassName?: string;
 }
 
-export default function InputFileField({
-  label,
-  labelClassName,
-  error,
-  caption,
-  cationClassName,
-  description,
-  descriptionClassName,
-  required,
-}: InputFileProps) {
-  return (
-    <div
-      id={label?.split(" ").join("-").toLowerCase()}
-      className="flex flex-col items-start justify-center w-full"
-    >
-      <label
-        className={`relative first-letter:capitalize block text-body3/medium  text-gray-500  truncate whitespace-nowrap ${labelClassName} ${
-          error ? "text-red-500" : ""
-        }`}
+const InputFileField = forwardRef(
+  (
+    {
+      label,
+      labelClassName,
+      error,
+      caption,
+      cationClassName,
+      description,
+      descriptionClassName,
+      required,
+      ...rest
+    }: InputFileProps,
+    ref
+  ) => {
+    return (
+      <div
+        id={label?.split(" ").join("-").toLowerCase()}
+        className="flex flex-col items-start justify-center w-full"
       >
-        {label}
-        {required && (
-          <span className="absolute font-light text-red-500 -top-1.5">
-            &lowast;
-          </span>
-        )}
-      </label>
-
-      <div className="flex mt-1 flex-col items-center cursor-pointer justify-center w-full h-24 border border-gray-300 border-dashed rounded-lg hover:bg-gray-50 pt-5 pb-6">
-        <p
-          className={classNames(
-            "mb-2 text-body3/regular text-gray-900",
-            cationClassName ? cationClassName : ""
-          )}
+        <label
+          className={`relative first-letter:capitalize block text-body3/medium  text-gray-500  truncate whitespace-nowrap ${labelClassName} ${
+            error ? "text-red-500" : ""
+          }`}
         >
-          {caption}
-        </p>
+          {label}
+          {required && (
+            <span className="absolute font-light text-red-500 -top-1.5">
+              &lowast;
+            </span>
+          )}
+        </label>
 
-        {description && (
+        <div className="flex mt-1 flex-col items-center cursor-pointer justify-center w-full h-24 border border-gray-300 border-dashed rounded-lg hover:bg-gray-50 pt-5 pb-6">
           <p
             className={classNames(
-              "text-label/regular text-gray-500",
-              descriptionClassName ? descriptionClassName : ""
+              "mb-2 text-body3/regular text-gray-900",
+              cationClassName ? cationClassName : ""
             )}
           >
-            {description}
+            {caption}
           </p>
-        )}
-      </div>
 
-      <input id={label} type="file" className="hidden" />
-    </div>
-  );
-}
+          {description && (
+            <p
+              className={classNames(
+                "text-label/regular text-gray-500",
+                descriptionClassName ? descriptionClassName : ""
+              )}
+            >
+              {description}
+            </p>
+          )}
+        </div>
+
+        <input
+          id={label}
+          {...rest}
+          ref={ref as LegacyRef<HTMLInputElement>}
+          type="file"
+          className="hidden"
+        />
+      </div>
+    );
+  }
+);
+
+export default InputFileField;
