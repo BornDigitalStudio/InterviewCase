191b9036ace356b62aff2631a8e1ae461e7a755f
Martin Bonchev <martin.bonchevv@gmail.com>
2023-05-03 00:43:33 +0300

    style: header responsive design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,20 +7,20 @@ const Header = forwardRef((_props, ref) => {
   return (
     <header
       ref={ref as React.LegacyRef<HTMLElement>}
-      className="bg-white  flex flex-row items-center justify-between  w-full h-16 px-10 py-3   border border-solid"
+      className="bg-white  flex flex-row items-center justify-between w-full h-16 gap-8 px-2 sm:px-10 py-3   border border-solid"
     >
       <InputField
-        contstyle="max-w-xs   border border-solid rounded"
+        contstyle="max-w-xs border border-solid rounded"
         inputClassName="h-10 focus:border-gray-500"
         prefix={<Icon icon="search" size={16} color="gray" />}
         placeholder="Search"
         inputMode="search"
       />
 
-      <div className="flex flex-row items-center gap-5">
+      <div className="flex flex-row items-center sm:gap-5 gap-4">
         <div className="flex flex-row items-center gap-2 cursor-pointer">
           <img src={USFlag} alt="US flag" className="h-5" />
-          USD
+          <p className="mr-4 sm:mr-0">USD</p>
         </div>
 
         <Icon icon="shopping-cart" size={20} color="gray" />
