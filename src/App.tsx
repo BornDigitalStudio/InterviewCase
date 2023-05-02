579b53e1f6fa35682980c7862e8725c2d405faa0
Martin Bonchev <martin.bonchevv@gmail.com>
2023-05-02 14:01:15 +0300

    feat: add TabContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
 import Header from "@components/Header";
+import TabContainer from "@components/TabContainer";
+import GeneralForm from "@components/GeneralForm";
+import { UserManagement } from "@components/UserManagement";
 
 function App() {
   return (
-    <div>
+    <div className="h-screen bg-gray-100">
       <Header />
+
+      <TabContainer title="Settings">
+        <TabContainer.Target containerId="general">General</TabContainer.Target>
+        <TabContainer.Target containerId="roles-management">
+          Roles management
+        </TabContainer.Target>
+        <TabContainer.Target defaultSelect containerId="user-management">
+          User management
+        </TabContainer.Target>
+        <TabContainer.Target containerId="notifications">
+          Notifications
+        </TabContainer.Target>
+
+        <TabContainer.Container id="general">
+          <GeneralForm />
+        </TabContainer.Container>
+
+        <TabContainer.Container id="roles-management">
+          <div>roles-management</div>
+        </TabContainer.Container>
+
+        <TabContainer.Container id="user-management">
+          <UserManagement />
+        </TabContainer.Container>
+
+        <TabContainer.Container id="notifications">
+          <div>notifications</div>
+        </TabContainer.Container>
+      </TabContainer>
     </div>
   );
 }
