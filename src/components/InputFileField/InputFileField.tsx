8ead49f738914e29e3f5f60bb027c1bde4611cf3
Martin Bonchev <martin.bonchevv@gmail.com>
2023-05-02 20:34:21 +0300

    fix: type of ref

--- src/components/InputFileField/InputFileField.tsx
+++ src/components/InputFileField/InputFileField.tsx
@@ -4,10 +4,10 @@ import type { FieldError } from "react-hook-form";
 import { classNames } from "@utils/functions/utils";
 
 interface InputFileProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  label: string;
+  ontoInputLabel: string;
+  ontoLabelClassName?: string;
+  label: React.ReactNode;
   labelClassName?: string;
-  caption: React.ReactNode;
-  cationClassName?: string;
   error?: FieldError | undefined;
   description?: React.ReactNode;
   descriptionClassName?: string;
@@ -16,45 +16,46 @@ interface InputFileProps extends React.InputHTMLAttributes<HTMLInputElement> {
 const InputFileField = forwardRef(
   (
     {
+      ontoInputLabel,
+      ontoLabelClassName,
+      error,
       label,
       labelClassName,
-      error,
-      caption,
-      cationClassName,
       description,
       descriptionClassName,
       required,
       ...rest
     }: InputFileProps,
-    ref
+    ref: LegacyRef<HTMLInputElement>
   ) => {
     return (
       <div
-        id={label?.split(" ").join("-").toLowerCase()}
+        id={ontoInputLabel?.split(" ").join("-").toLowerCase()}
         className="flex flex-col items-start justify-center w-full"
       >
-        <label
-          className={`relative first-letter:capitalize block text-body3/medium  text-gray-500  truncate whitespace-nowrap ${labelClassName} ${
+        <p
+          className={`relative first-letter:capitalize block text-body3/medium  text-gray-500  truncate whitespace-nowrap ${ontoLabelClassName} ${
             error ? "text-red-500" : ""
           }`}
         >
-          {label}
+          {ontoInputLabel}
           {required && (
             <span className="absolute font-light text-red-500 -top-1.5">
               &lowast;
             </span>
           )}
-        </label>
+        </p>
 
         <div className="flex mt-1 flex-col items-center cursor-pointer justify-center w-full h-24 border border-gray-300 border-dashed rounded-lg hover:bg-gray-50 pt-5 pb-6">
-          <p
+          <label
+            htmlFor={ontoInputLabel}
             className={classNames(
-              "mb-2 text-body3/regular text-gray-900",
-              cationClassName ? cationClassName : ""
+              "mb-2 text-body3/regular cursor-pointer text-gray-900",
+              labelClassName ? labelClassName : ""
             )}
           >
-            {caption}
-          </p>
+            {label}
+          </label>
 
           {description && (
             <p
@@ -66,15 +67,15 @@ const InputFileField = forwardRef(
               {description}
             </p>
           )}
-        </div>
 
-        <input
-          id={label}
-          {...rest}
-          ref={ref as LegacyRef<HTMLInputElement>}
-          type="file"
-          className="hidden"
-        />
+          <input
+            id={ontoInputLabel}
+            {...rest}
+            ref={ref}
+            type="file"
+            className="hidden"
+          />
+        </div>
       </div>
     );
   }
