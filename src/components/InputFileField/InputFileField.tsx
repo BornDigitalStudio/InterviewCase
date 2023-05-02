11fdd8531fb68e90888fdba10715479401ba85e8
Martin Bonchev <martin.bonchevv@gmail.com>
2023-05-02 17:12:36 +0300

    feat: add id to input

--- src/components/InputFileField/InputFileField.tsx
+++ src/components/InputFileField/InputFileField.tsx
@@ -61,7 +61,7 @@ export default function InputFileField({
         )}
       </div>
 
-      <input type="file" className="hidden" />
+      <input id={label} type="file" className="hidden" />
     </div>
   );
 }
